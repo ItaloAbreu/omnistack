452a850ccd30fa1e33aa62c55410cb3c5b95c280
Italo Abreu <italodw@gmail.com>
2019-06-18 00:59:23 -0300

    Styled Component

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -3,7 +3,8 @@ import api from '../services/api';
 import io from 'socket.io-client';
 import config from '../config';
 
-import './Feed.css';
+// import './Feed.css';
+import { PostList } from './FeedStyles';
 
 import more from '../assets/more.svg';
 import like from '../assets/like.svg';
@@ -47,7 +48,7 @@ class Feed extends Component {
 
     render() {
         return (
-            <section id="post-list">
+            <PostList>
                 {this.state.feed.map(post => (
                     <article key={post._id} >
                         <header>
@@ -79,7 +80,7 @@ class Feed extends Component {
                         </footer>
                     </article>
                 ))}
-            </section>
+            </PostList>
         );
     }
 }
